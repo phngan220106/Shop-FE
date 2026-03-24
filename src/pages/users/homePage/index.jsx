571766c0d98b1c3cc7f8bab18a5a5df405a2ea61
TOVIETNGAN <24521134@gm.uit.edu.vn>
2026-03-25 01:17:12 +0700

    SetFiles

--- src/pages/users/homePage/index.jsx
+++ src/pages/users/homePage/index.jsx
@@ -1,7 +1,13 @@
 import { memo } from 'react';
+import Header from '../theme/header/index.jsx';
+
 const HomePage = () => {
     return (
-        <h1>HomePage</h1>
+        <>
+            <Header />
+            <h2>HomePage</h2>
+            <h2>Footer</h2>
+        </>
     );
 };
 export default memo(HomePage);
---
+++ src/pages/users/theme/header/index.jsx
@@ -0,0 +1,12 @@
+import { memo } from "react";
+import './style.scss';
+const Header = () => {
+    return (
+        <div className="header_top">
+            <div className="container">
+                Header
+            </div>
+        </div>
+    );
+}
+export default memo(Header);
