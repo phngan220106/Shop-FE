432761015ef55c9619782ac60ab3ba88438bed46
TOVIETNGAN <24521134@gm.uit.edu.vn>
2026-03-25 00:11:52 +0700

    setUpFiles

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import HomePage from './pages/users/homePage/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
